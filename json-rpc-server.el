041b0a70d692a3713ed7dacf2918f93283c98c1a
jcaw <40725916+jcaw@users.noreply.github.com>
2019-05-22 08:11:32 -0700

    Add objects that represent the JSON-RPC request & response.
    Also add an implementation-specific variant of the request object, which stores
    the inbound connection from elnode.

--- json-rpc-server.el
+++ json-rpc-server.el
@@ -218,6 +218,75 @@ its own function to enable custom body handling."
           (t (jrpc--apply-exposed-function func-symbol args-as-list httpcon)))))
 
 
+(cl-defstruct jrpc-request
+  "Object representing a JSON-RPC request.
+
+This represents a request as laid out in the JSON-RPC 2.0
+specification.
+
+https://www.jsonrpc.org/specification"
+  json-version
+  method
+  (params '())
+  id)
+
+
+(cl-defstruct (jrpc-call (:include jrpc-request))
+  "Object representing a remote procedure call.
+
+This contains each component of the json-rpc protocol, as well as
+information about the HTTP connection.
+
+The member `HTTPCON' holds an Elnode `httpcon' object. This
+represents the inbound connection.
+
+All other members are inherited from `jrpc-request'."
+  httpcon)
+
+
+(cl-defstruct jrpc-error
+  "Object representing a JSON-RPC error."
+  (code :type int)
+  (message :type string)
+  (data nil))
+
+
+(cl-defstruct (jrpc-response
+               (:constructor nil)
+               (:constructor make-jrpc-response-error
+                             (&key
+                              (error :type jrpc-eror)
+                              id)
+                             "Construct a JSON-RPC response for an error.")
+               (:constructor make-jrpc-response-result
+                             (&key result id)
+                             "Construct a JSON-RPC response for a successful result."))
+  "Object representing a JSON-RPC response.
+
+This represents a response as laid out in the JSON-RPC 2.0
+specification.
+
+https://www.jsonrpc.org/specification
+
+Note that a response can contain *either* the result of a
+successful RPC call or an error, but not both. Thus this struct
+has two constructors:
+
+ - `make-jrpc-response-result' :: create a response representing
+                                  a successful result.
+ - `make-jrpc-response-error' :: create a response representing
+                                 an error.
+
+No other constructors exist. One of those two must be used.
+
+Also note that this response will *always* be tagged as JSON-RPC
+2.0, even if the client sent a JSON-RPC 1.0 request."
+  (json-version "2.0")
+  (result nil)
+  (error nil)
+  id)
+
+
 (defun jrpc-expose-function (func)
   "Expose a function to \"/eval-function\".
 
