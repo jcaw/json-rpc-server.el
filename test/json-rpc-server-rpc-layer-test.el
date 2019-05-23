6849a2e1509607159ed9e2a85b8743846e1bff5a
jcaw <40725916+jcaw@users.noreply.github.com>
2019-05-23 11:42:54 -0700

    Add tests directory
    Also add a test file for "json-rpc-server-rpc-layer.el". Initially, this just
    tests the function `jrpc--request-from-json`.

---
+++ test/json-rpc-server-rpc-layer-test.el
@@ -0,0 +1,85 @@
+;;; json-rpc-server-rpc-layer-test.el --- Tests for json-rpc-server
+
+
+(require 'ert)
+
+(load-file "json-rpc-server-rpc-layer.el")
+
+
+;; Unit tests
+(progn
+  (ert-deftest test-jrpc--request-from-json ()
+    "Test class for `jrpc--request-from-json'.
+
+Test whether it decodes request json properly, and raises the
+correct errors for flawed json.
+
+This test doesn't thoroughly test the validity of the json. That
+is handled in the unit test for the json decoder itself.
+
+Note that when testing for raised errors, it doesn't test error messages - it just tests the class of the signal."
+    ;; Valid request, all fields filled.
+    (should (jrpc--structs-equal
+             (jrpc--request-from-json "{\"jsonrpc\": \"2.0\",\"method\": \"message\",\"params\": [\"This is a %s\", \"test message\"],\"id\": 12456,}")
+             (make-jrpc-request :jsonrpc "2.0"
+                                :method "message"
+                                :params '("this is a %s" "test message")
+                                :id 12456)))
+    ;; Valid request, but it's jsonrpc 1.0
+    (should (jrpc--structs-equal
+             (jrpc--request-from-json
+              "{\"method\": \"message\",\"params\": [\"This is a %s\", \"test message\"],\"id\": 12456,}")
+             (make-jrpc-request :jsonrpc nil
+                                :method "message"
+                                :params '("this is a %s" "test message")
+                                :id 12456)))
+    ;; Valid request, but there's no params.
+    (should (jrpc--structs-equal
+             (jrpc--request-from-json "{\"jsonrpc\": \"2.0\",\"method\": \"message\",\"id\": 12456,}")
+             (make-jrpc-request :jsonrpc "2.0"
+                                :method "message"
+                                :params '()
+                                :id 12456)))
+
+    ;; Invalid `jsonrpc' param
+    (progn
+      ;; jsonrpc is 3.0 - too high
+      (should-error (jrpc--request-from-json "{\"jsonrpc\": \"3.0\",\"method\": \"message\",\"id\": 12456,}")
+                    :type 'jrpc-invalid-request)
+      ;; jsonrpc is 2 -- formatted wrong
+      (should-error (jrpc--request-from-json "{\"jsonrpc\": \"2\",\"method\": \"message\",\"id\": 12456,}")
+                    :type 'jrpc-invalid-request))
+
+    ;; Invalid `method' param
+    (progn
+      ;; No method
+      (should-error (jrpc--request-from-json "{\"jsonrpc\": \"2.0\",\"id\": 12456,}")
+                    :type 'jrpc-invalid-request)
+      ;; Wrong type for method
+      (should-error (jrpc--request-from-json "{\"jsonrpc\": \"2.0\",\"method\": 120983,\"id\": 12456,}")
+                    :type 'jrpc-invalid-request))
+
+    ;; Invalid `params' param
+    (progn
+      (should-error (jrpc--request-from-json "{\"jsonrpc\": \"2.0\",\"method\": \"message\",\"params\": \"Just a string param\",\"id\": 12456,}")
+                    :type 'jrpc-invalid-request))
+
+    ;; Invalid `id' param
+    (progn
+      ;; No id
+      (should-error (jrpc--request-from-json "{\"jsonrpc\": \"2.0\",\"method\": \"message\",}")
+                    :type 'jrpc-invalid-request)
+      ;; Invalid id type - in this case, a string.
+      (should-error (jrpc--request-from-json "{\"jsonrpc\": \"2.0\",\"method\": \"message\",\"id\": \"12456\",}")
+                    :type 'jrpc-invalid-request))))
+
+
+;; Integration tests
+(progn
+  ;; None yet.
+  )
+
+
+;; TODO: Do we need to provide?
+;; (provide 'json-rpc-server-rpc-layer-test)
+;;; json-rpc-server-rpc-layer-test.el ends here
---
+++ test/test-helper.el
@@ -0,0 +1,40 @@
+;;; test-helper.el --- Helpers for json-rpc-server-test.el
+
+
+(defun jrpc--structs-equal (instance1 instance2)
+  "Compare two structs. Are they equal?
+
+Equality here means all of the following are true:
+
+  - Both `INSTANCE1' and `INSTANCE2' are the same type of struct
+
+  - Every slot in `INSTANCE1' is equal to the same slot in
+    `INSTANCE2', according to cl-equal.
+
+An error will be raised if a type other than `cl-struct' is
+provided.
+
+This method gets around a quirk in normal struct comparison where
+two equivalent structs will not count as equal."
+  (unless (cl-struct-p instance1)
+    (error "`instance1' not a cl-struct"))
+  (unless (cl-struct-p instance2)
+    (error "`instance2' not a cl-struct"))
+  (and
+   ;; Ensure they're both the same type of object
+   (eq (type-of instance1)
+       (type-of instance2))
+   ;; Ensure every property is the same.
+   (let ((struct-type
+          ;; Remember - they have the same type.
+          (type-of instance1)))
+     (cl-every
+      (lambda (slot-pair)
+        ;; Ensure this slot is the same in both instances
+        (let ((slot-name (car slot-pair)))
+          (cl-equalp (cl-struct-slot-value struct-type slot-name instance1)
+                     (cl-struct-slot-value struct-type slot-name instance2))))
+      ;; Map over all slot names
+      (cdr (cl-struct-slot-info struct-type))))))
+
+;;; test-helper.el ends here
