be694f577649d164565e44829e5c517a18022642
jcaw <40725916+jcaw@users.noreply.github.com>
2019-05-23 14:30:22 -0700

    Moves struct definitions to top of file

--- json-rpc-server-rpc-layer.el
+++ json-rpc-server-rpc-layer.el
@@ -33,6 +33,83 @@ will not be executed.")
   "A variable had the wrong type.")
 
 
+(cl-defstruct jrpc-request
+  "Object representing a JSON-RPC request.
+
+This represents a request as laid out in the JSON-RPC 2.0
+specification.
+
+https://www.jsonrpc.org/specification"
+  jsonrpc
+  method
+  (params '())
+  id)
+
+
+(cl-defstruct jrpc-error
+  "Object representing a JSON-RPC error."
+  (code (:type int))
+  (message (:type string))
+  (data nil))
+
+
+(cl-defstruct (jrpc-response
+               (:constructor nil)
+               (:constructor make-jrpc-response-error
+                             (&key
+                              (error (:type jrpc-eror))
+                              id)
+                             "Construct a JSON-RPC response for an error.")
+               (:constructor make-jrpc-response-result
+                             (&key result id)
+                             "Construct a JSON-RPC response for a successful result."))
+  "Object representing a JSON-RPC response.
+
+This represents a response as laid out in the JSON-RPC 2.0
+specification.
+
+https://www.jsonrpc.org/specification
+
+Note that a response can contain *either* the result of a
+successful RPC call or an error, but not both. Thus this struct
+has two constructors:
+
+ - `make-jrpc-response-result' :: create a response representing
+                                  a successful result.
+ - `make-jrpc-response-error' :: create a response representing
+                                 an error.
+
+No other constructors exist. One of those two must be used.
+
+Also note that this response will *always* be tagged as JSON-RPC
+2.0, even if the client sent a JSON-RPC 1.0 request."
+  (jsonrpc "2.0")
+  (result nil)
+  (error nil)
+  id)
+
+
+(defun jrpc-response-to-alist (instance)
+  "Convert a jrpc response object into an alist so it can be encoded into JSON."
+  (list (cons "jsonrpc" (jrpc-response-jsonrpc instance))
+        (cons "result" (jrpc-response-result instance))
+        (cons "error" (jrpc-error-to-alist (jrpc-response-error instance)))
+        (cons "id" (jrpc-response-id instance))))
+
+
+(defun jrpc-error-to-alist (instance)
+  "Convert a jrpc error object into an alist so it can be encoded into JSON.
+
+If instance is nil, nil will be returned."
+  (when instance
+    (list (cons "code" (jrpc-error-code instance))
+          (cons "message" (jrpc-error-message instance))
+          (cons "data" (jrpc-error-data instance)))))
+
+
+;; --------------------------------------------------------------------------
+
+
 (defun jrpc-null-p (value)
   "Is `VALUE' either nil, or json-null?
 
@@ -198,80 +275,6 @@ is transport-agnostic - transport has to be implemented
 separately."
   (jrpc--execute-request
    (jrpc--request-from-json)))
-
-
-(cl-defstruct jrpc-request
-  "Object representing a JSON-RPC request.
-
-This represents a request as laid out in the JSON-RPC 2.0
-specification.
-
-https://www.jsonrpc.org/specification"
-  jsonrpc
-  method
-  (params '())
-  id)
-
-
-(cl-defstruct jrpc-error
-  "Object representing a JSON-RPC error."
-  (code (:type int))
-  (message (:type string))
-  (data nil))
-
-
-(cl-defstruct (jrpc-response
-               (:constructor nil)
-               (:constructor make-jrpc-response-error
-                             (&key
-                              (error (:type jrpc-eror))
-                              id)
-                             "Construct a JSON-RPC response for an error.")
-               (:constructor make-jrpc-response-result
-                             (&key result id)
-                             "Construct a JSON-RPC response for a successful result."))
-  "Object representing a JSON-RPC response.
-
-This represents a response as laid out in the JSON-RPC 2.0
-specification.
-
-https://www.jsonrpc.org/specification
-
-Note that a response can contain *either* the result of a
-successful RPC call or an error, but not both. Thus this struct
-has two constructors:
-
- - `make-jrpc-response-result' :: create a response representing
-                                  a successful result.
- - `make-jrpc-response-error' :: create a response representing
-                                 an error.
-
-No other constructors exist. One of those two must be used.
-
-Also note that this response will *always* be tagged as JSON-RPC
-2.0, even if the client sent a JSON-RPC 1.0 request."
-  (jsonrpc "2.0")
-  (result nil)
-  (error nil)
-  id)
-
-
-(defun jrpc-response-to-alist (instance)
-  "Convert a jrpc response object into an alist so it can be encoded into JSON."
-  (list (cons "jsonrpc" (jrpc-response-jsonrpc instance))
-        (cons "result" (jrpc-response-result instance))
-        (cons "error" (jrpc-error-to-alist (jrpc-response-error instance)))
-        (cons "id" (jrpc-response-id instance))))
-
-
-(defun jrpc-error-to-alist (instance)
-  "Convert a jrpc error object into an alist so it can be encoded into JSON.
-
-If instance is nil, nil will be returned."
-  (when instance
-    (list (cons "code" (jrpc-error-code instance))
-          (cons "message" (jrpc-error-message instance))
-          (cons "data" (jrpc-error-data instance)))))
 
 
 (defun jrpc-expose-function (func)
