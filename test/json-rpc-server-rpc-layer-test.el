0379621e0469e298258afe6d4bfd37eb233f3f94
jcaw <40725916+jcaw@users.noreply.github.com>
2019-05-23 13:20:38 -0700

    Add tests for `jrpc--decode-request-json`

--- test/json-rpc-server-rpc-layer-test.el
+++ test/json-rpc-server-rpc-layer-test.el
@@ -72,7 +72,40 @@ Note that when testing for raised errors, it doesn't test error messages - it ju
                     :type 'jrpc-invalid-request)
       ;; Invalid id type - in this case, a string.
       (should-error (jrpc--request-from-json "{\"jsonrpc\": \"2.0\",\"method\": \"message\",\"id\": \"12456\",}")
-                    :type 'jrpc-invalid-request))))
+                    :type 'jrpc-invalid-request)))
+
+  (ert-deftest test-jrpc--decode-request-json ()
+    "Test for `jrpc--decode-request-json'.
+
+Test whether it decodes json correctly, in the way I want.
+
+Note that this does not test the functionality of `json.el'. It
+only tests the additional conditions imposed by the
+`jrpc--decode-request-json' method." )
+  ;; List decoding
+  (progn
+    ;; Simple members
+    (should (equal (jrpc--decode-request-json
+                    "[1, 2, 3]")
+                   '(1 2 3)))
+    (should (equal (jrpc--decode-request-json
+                    "[\"first\", \"second\", \"third\"]")
+                   '("first" "second" "third"))))
+
+  ;; Index and object decoding.
+  ;;
+  ;; Indexes should be symbols, and the result should be an alist.
+  (should (equal (jrpc--decode-request-json
+                  "{\"index1\": \"value1\", \"index2\": \"value2\"}")
+                 '((index1 . "value1")
+                   (index2 . "value2"))))
+
+  ;; Malformed json should raise a specific error, so it can be caught.
+  (should-error (jrpc--decode-request-json
+                 ;; Some malformed JSON input.
+                 "als;d'asfoasf")
+                :type 'json-readtable-error)
+  )
 
 
 ;; Integration tests
