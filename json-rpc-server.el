d8ff3bd2d59b5cbaa086f96d759b8f8ba57629d7
jcaw <40725916+jcaw@users.noreply.github.com>
2019-06-06 10:41:13 -0700

    Add package requirements

--- json-rpc-server.el
+++ json-rpc-server.el
@@ -6,6 +6,7 @@
 ;; URL: https://github.com/jcaw/json-rpc-server.el
 ;; Keywords: tools, comm
 ;; Version: 0.1.0
+;; Package-Requires: ((emacs "26"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
