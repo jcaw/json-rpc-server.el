39cd44e93c9e826f842efdcd5f949a02e623add2
jcaw <40725916+jcaw@users.noreply.github.com>
2019-06-06 10:24:01 -0700

    Add package header

--- json-rpc-server.el
+++ json-rpc-server.el
@@ -1,6 +1,70 @@
-;; -*- lexical-binding: t -*-
+;;; json-rpc-server.el --- Server-side JSON-RPC library. Execute JSON-RPC requests in Emacs.   -*- lexical-binding: t; -*-
 
-;; Package that allows external JSON-RPC calls to Emacs.
+;; Copyright (C) 2019  GitHub user "Jcaw"
+
+;; Author: GitHub user "Jcaw"
+;; Keywords: tools, comm
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is a full implementation of the JSON-RPC 2.0 protocol[1] for Emacs. You
+;; pass in a JSON string, Emacs executes the specified function(s), and a
+;; JSON-RPC 2.0 response is returned.
+
+;; It was originally part of Porthole, a full-fledged HTTP-based RPC server for
+;; Emacs:
+
+;; http://www.github.com/jcaw/porthole
+
+;; The underlying JSON-RPC protocol was extracted into a separate package so
+;; that it could serve as a framework on which other RPC servers could be built.
+
+;; *No transport logic is included.* This package is designed to sit underneath
+;; a transport layer, which is responsible for communicate with external
+;; clients. Since JSON-RPC provides no inbuilt mechanism for authenticating
+;; requests, the transport layer should also handle authentication.
+
+;; Here's en example request:
+
+;; {
+;;   "jsonrpc": "2.0",
+;;   "method": "insert",
+;;   "params": ["some text to insert"],
+;;   "id": "3140983184"
+;; }
+
+;; Pass this as a string to `jrpc-handle' - the specified text will be inserted
+;; and the return value of the call to `insert' will be encoded into a response.
+
+;; Symbols and keywords can be passed by abusing the JSON-RPC syntax as follows:
+
+;; {
+;;   "a symbol": "'a-symbol",
+;;   "a keyword": ":a-keyword"
+;; }
+
+;; This is a simplified explanation of the package that glosses over a lot of
+;; details. Please refer to the full README[3] for an up-to-date and thorough
+;; explanation.
+
+;; [1] https://www.jsonrpc.org/specification
+
+;; [2] https://groups.google.com/d/msg/json-rpc/PN462g49yL8/DdMa93870_o
+
+;; [3] http://www.github.com/jcaw/json-rpc-server.el/README.md
 
 
 ;;; Code:
@@ -282,6 +346,7 @@ exist (or has not been exposed.)"
     ;; We now check that the function is legal, and callable, before trying to
     ;; call it.
     (unless (member method-symbol exposed-functions)
+      (message "Got \"%s\" of type %s. Exposed are: %s" method-symbol (type-of method-symbol) exposed-functions)
       (jrpc--raise-procedural-error
        'jrpc-invalid-function
        (concat
